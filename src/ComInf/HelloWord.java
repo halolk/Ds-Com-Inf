5a163b3397ac1235580d7c7b0a1cd7fef861a956
halolk <kelicmu@gmail.com>
2014-01-22 11:39:10 -0500

    test

--- src/ComInf/HelloWord.java
+++ src/ComInf/HelloWord.java
@@ -1,9 +1,13 @@
 package ComInf;
 
+import java.util.Arrays;
+
 public class HelloWord {
 
 	public static void main(String[] args) {
+		int[] arr = {1,2,4,8,16};
 		System.out.println("hello");
+		System.out.println(Arrays.toString(arr));
 
 	}
 
